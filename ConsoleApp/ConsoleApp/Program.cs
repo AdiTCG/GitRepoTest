5943349c073057f5656828d04597c29cf0211b16
Subir <cad.customizations@gmail.com>
2018-06-21 20:08:47 +0530

    test1

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello india");
         }
     }
 }
